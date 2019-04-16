c6d4b838f86e347c00ba6b3b5d4f895197293e02
Vinicius Heitor <viniciusheitor14@gmail.com>
2019-04-16 14:42:34 -0300

    finished the Post model and Posts controller - missing file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,40 @@
 class PostsController < ApplicationController
+
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+        redirect_to @post, notice: "The post was created!"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+        redirect_to @post, notice: "The post was updated!"
+    else
+      render 'edit'
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path, notice "Post destroyed"
   end
 
 private
@@ -29,5 +46,4 @@ private
   def find_post
     @post = Post.find(params[:id])
   end
-
 end
