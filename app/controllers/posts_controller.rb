5afb497aa707c99c50ec4e119380e422813b7eb0
Vinicius Heitor <viniciusheitor14@gmail.com>
2019-04-16 16:33:22 -0300

    Added CRUD and categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,12 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    if params[:category].blank?
+      @posts = Post.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @posts = Post.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -30,6 +35,7 @@ class PostsController < ApplicationController
         redirect_to @post, notice: "The post was updated!"
     else
       render 'edit'
+    end
   end
 
   def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :category
-  validates :title, :content, :contegoryId, presence:true
+  validates :title, :content, :contegoryId, presence: true
+  has_many :line_items, inverse_of: :order
 end
